6acedb4e07823afe116721cf9290a3e90f5a8c7e
Ophirus <ophir.haramaty@innovesta.co>
2017-12-19 19:09:10 +0200

    luanch on server

--- server.py
+++ server.py
@@ -1,6 +1,14 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, request
+import sys,subprocess
+from pytube import YouTube
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+app = Flask(__name__)
+@app.route('/youtube', methods=['POST'])
+def fromYoutubeToMega():
+	link = request.args.get("link")
+	print("downloading from link: " + link)
+	YouTube(link).streams.filter(progressive=True, file_extension='mp4').first().download()
+	print ("uploading to Mega...\n")
+	subprocess.call('sh toMega.sh', shell=True)
+	return 'uploaded clip from link ' + link + '\n'
+
