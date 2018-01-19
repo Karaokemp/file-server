68bfce86ff1fb08869d951f2b9a3fec5edfa55b9
Ophirus <ophir.haramaty@innovesta.co>
2018-01-19 18:28:44 +0200

    fix to python3

--- run.py
+++ run.py
@@ -5,8 +5,8 @@ from pytube import YouTube
 
 address = sys.argv[1]
 
-print "Downloading from youtube Link: " +address
+print ("Downloading from youtube Link: " + address)
 YouTube(address).streams.filter(progressive=True, file_extension='mp4').first().download()
 
-print "aploading to Mega..."
+print ("uploading to Mega...")
 subprocess.call('sh toMega.sh', shell=True)
