7b31b3a8b4cf8f1499aa73efab9667c39538060d
Ophirus <ophir.haramaty@innovesta.co>
2018-01-21 02:30:21 +0200

    fix cors ability

--- server.py
+++ server.py
@@ -1,8 +1,10 @@
 from flask import Flask, request
+from flask_cors import CORS
 import sys,subprocess
 from pytube import YouTube
 
 app = Flask(__name__)
+CORS(app)
 @app.route('/youtube', methods=['POST'])
 def fromYoutubeToMega():
 	link = request.args.get("link")
